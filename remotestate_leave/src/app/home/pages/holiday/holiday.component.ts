c0a22e7659700b6dff046738cb4fdaf355e74d5b
Rajatagknp <Rajat.agarwal@remotestate.com>
2021-06-14 15:53:42 +0530

    leave

--- remotestate_leave/src/app/home/pages/holiday/holiday.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "holiday",
-    styleUrls:["holiday.component.scss"],
-    templateUrl:"./holiday.component.html"
-})
-
-export class holidaycomponent {}
